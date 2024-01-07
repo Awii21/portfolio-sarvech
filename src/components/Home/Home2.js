ca406e9eae4906d59f7325d921b08c1b4b7fb63f
Awais <awaissmr@gmail.com>
2024-01-07 22:51:27 +0500

    photo added

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.png";
+import myImg from "../../Assets/avatar.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -86,26 +86,33 @@ function Home2() {
                   <RiTwitterXFill />
                 </a>
               </li> */}
-             <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/i-will-see-you-358b9b241/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/sheedi_sarveech?utm_source=qr&r=nametag"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
+
+
+    
+
+
+
+
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/i-will-see-you-358b9b241/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/sheedi_sarveech?utm_source=qr&r=nametag"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
             </ul>
           </Col>
         </Row>
